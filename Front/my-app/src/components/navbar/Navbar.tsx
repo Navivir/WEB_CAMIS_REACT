d5a38fe95c495ea97d51d4cc8d1c3274e1584131
Navivir <162104416+Navivir@users.noreply.github.com>
2024-10-28 11:31:21 +0100

    añadido logo perfil a main

--- Front/my-app/src/components/navbar/Navbar.tsx
+++ Front/my-app/src/components/navbar/Navbar.tsx
@@ -1,7 +1,6 @@
-"use client";
-
 import { Button, Navbar } from "flowbite-react";
 import logo from '../../camiB.svg';
+import profileIcon from '../../profileW.svg'; // Importa el ícono de perfil
 import { Link } from 'react-router-dom';
 
 export function NavbarW() {
@@ -11,8 +10,14 @@ export function NavbarW() {
         <img src={logo} className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold text-white">UniqTee</span>
       </Navbar.Brand>
-      <div className="flex md:order-2">
+      <div className="flex items-center space-x-4 md:order-2">
         <Button>See Our Products</Button>
+        
+        {/* Enlace al perfil con ícono */}
+        <Link to="/profile">
+          <img src={profileIcon} alt="Profile" className="h-6 w-6" />
+        </Link>
+        
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
